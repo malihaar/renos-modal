806f96daefc08205ef9cd22eb8c1f2ccfc5f382f
Maliha Rahmani <amaliharahmani@gmail.com>
2022-08-31 14:18:52 +0700

    add vanillajs functionality

--- vanillajs/app.js
+++ vanillajs/app.js
@@ -5,26 +5,15 @@ var backdrop = document.querySelector('.backdrop');
 var textEdit = document.querySelector('.modal textarea');
 
 
-
-function updateParagraph() {
-  outputParagraph.textContent = quote;
-}
-
 function closeModal() {
   modal.style.display = 'none';
   backdrop.style.display = 'none';
 }
 
-
-
-buttons[2].addEventListener('click', function() {
+buttons[1].addEventListener('click', function() {
   modal.style.display = 'block';
   backdrop.style.display = 'block';
 });
 
 buttons[0].addEventListener('click', closeModal);
 backdrop.addEventListener('click', closeModal);
-
-buttons[1].addEventListener('click', function() {
-  closeModal();
-});
